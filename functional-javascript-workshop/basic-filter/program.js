91853cf31a035151716fc79c4146d349e8c8f645
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-15 17:10:14 -0400

    Continue to develop basic-filter/program.js

--- functional-javascript-workshop/basic-filter/program.js
+++ functional-javascript-workshop/basic-filter/program.js
@@ -1,5 +1,15 @@
 function getShortMessages(messages) {
   // SOLUTION GOES HERE
-}
+  // console.log(messages)
+  var lengths = messages.map(function (message) {
+    console.log(message.message)
+    //console.log((message.message.length < 50) ? 1 : 0)
+    return (message.message.length < 50) ? true : false
+  })
+  console.log(lengths)
 
+  //   .filter(function (message) {
+  //   if (message) return message
+  // })
+}
 module.exports = getShortMessages
