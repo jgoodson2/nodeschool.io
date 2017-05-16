c64f38ebe62f316d4af67d8938c6facee9de497e
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-16 10:15:23 -0400

    Add official solution

--- functional-javascript-workshop/basic-filter/program.js
+++ functional-javascript-workshop/basic-filter/program.js
@@ -7,3 +7,16 @@ function getShortMessages(messages) {
   })
 }
 module.exports = getShortMessages
+
+/*
+ Here's the official solution in case you want to compare notes:
+
+ ────────────────────────────────────────────────────────────────────────────────
+ module.exports = function getShortMessages(messages) {
+ return messages.filter(function(item) {
+ return item.message.length < 50
+ }).map(function(item) {
+ return item.message
+ })
+ }
+ */
