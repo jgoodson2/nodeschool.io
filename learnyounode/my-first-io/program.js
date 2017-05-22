5c8678cc9990bb78a3ce4647a52c03055bff33a9
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-22 14:15:00 -0400

    Add an equivalent solution to learnyounode/my-first-io

--- learnyounode/my-first-io/program.js
+++ learnyounode/my-first-io/program.js
@@ -1,3 +1,6 @@
 var fs = require('fs')
 
 console.log(fs.readFileSync(process.argv[2]).toString().split('\n').length - 1)
+
+// Another way to do it
+// console.log(fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1)
