8b36024abd0cbfff59fdd006b8152d5f6311fcb7
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-23 09:23:22 -0400

    Add solution for learnyounode/filtered-ls

--- learnyounode/filtered-ls/program.js
+++ learnyounode/filtered-ls/program.js
@@ -1,13 +1,10 @@
 var fs = require('fs')
+var path = require('path')
 
 fs.readdir(process.argv[2], function (err, list) {
   var pattern = process.argv[3].trim()
-  //console.log('pattern = ' + pattern)
   if (err) return console.error(err)
   list.forEach(function (item) {
-    // console.log(item + ' ' + item.trim().indexOf(pattern))
-    if (item.trim().indexOf(pattern) >= 0) {
-      console.log(item)
-    }
+    if (path.extname(item) == '.' + pattern) console.log(item)
   })
 })
